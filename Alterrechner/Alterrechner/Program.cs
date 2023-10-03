2c21cfe9325572a93aa1da17caf6afde45370dc2
Marcel Kainz <m.kainz1@htlkrems.at>
2023-10-03 11:57:16 +0200

    12:00

---
+++ Alterrechner/Alterrechner/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int age;
+int year = DateTime.Now.Year;
+int birthyear = 2008;
+Console.WriteLine("Wann bist du geboren (Jahr)");
+Console.Read();
+age = year - birthyear;
+Console.WriteLine(age);
