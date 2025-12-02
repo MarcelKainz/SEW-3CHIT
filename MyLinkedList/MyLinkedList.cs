5b8d71bce2bf01bfbddcbc23601f31ed4770b7c1
Marcel Kainz <m.kainz1@htlkrems.at>
2025-12-02 12:11:18 +0100

    MyLinkedList

--- MyLinkedList/MyLinkedList.cs
+++ MyLinkedList/MyLinkedList.cs
@@ -4,12 +4,15 @@
 public class MyLinkedList
 {
     public MyElement Head { get; private set; }
+    public int Count { get; private set; }
 
     public void AddFirst(int value)
     {
         MyElement tmp = new MyElement { Value = value };
         tmp.Next = Head;
         Head = tmp;
+        
+        Count++;
     }
 
     public void AddLast(int value)
@@ -26,38 +29,40 @@ public class MyLinkedList
 
             tmp.Next = new MyElement{Value = value};
         }
+
+        Count++;
     }
 
     public void Remove(int value)
     {
+        MyElement tmp = Head;
+        
         if (Head == null)
             return;
 
         if (Head.Value == value)
         {
             Head = Head.Next;
+            Count++;
             return;
         }
 
-        MyElement tmp = Head;
-        MyElement current = Head.Next;
-        while (current != null)
+        while (tmp.Next != null && tmp.Next.Value != value)
         {
-            if (current.Value == value)
-            {
-                tmp.Next = current.Next;
-                return;
-            }
+            tmp = tmp.Next;
+        }
 
-            tmp = current;
-            current = current.Next;
+        if (tmp.Next != null)
+        {
+            tmp.Next = tmp.Next.Next;
+            Count++;
         }
     }
     
     
     public override string ToString()
     {
-        return Head.ToString();
+        return $"{Count} Elements: ->" + Head.ToString();
     }
 }
 
--- MyLinkedList/Program.cs
+++ MyLinkedList/Program.cs
@@ -1,13 +1,18 @@
 ﻿using MyList;
 
 MyLinkedList ml = new MyLinkedList();
+
+ml.AddFirst(3);
 ml.AddFirst(17);
 ml.AddFirst(7);
-ml.AddFirst(3);
+ml.AddFirst(7);
+ml.AddLast(44);
 
-ml.Remove(3);
+Console.WriteLine(ml);
+
 ml.Remove(7);
-ml.Remove(17);
+Console.WriteLine(ml);
 
+ml.Remove(99);
 Console.WriteLine(ml);
 
