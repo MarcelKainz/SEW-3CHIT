0146a5120106b02f5337aaee85e7f372e7abd3aa
Marcel Kainz <m.kainz1@htlkrems.at>
2026-03-03 12:46:59 +0100

    CRUD

---
+++ ScottConsolenAnwendung2.1/ScottConsolenAnwendung2.1/DBAccess.cs
@@ -0,0 +1,97 @@
+﻿using System.Data;
+using System.Text;
+
+namespace ScottConsolenAnwendung2._1;
+using MySqlConnector;
+
+public class DBAccess
+{
+
+
+    //Server=myServerAddress;Database=myDataBase;uid=myUsername;Pwd=myPassword;
+
+    //root = "quick and dirty" - normalerweise anderer User
+    string connectionString = "Server=localhost;Database=scottnew;Uid=root;Pwd=insy;";
+
+    public string GetAllDepts()
+    {
+        using (MySqlConnection mc = new MySqlConnection(connectionString))
+        {
+            mc.Open();
+            MySqlCommand cmd = new MySqlCommand("Select * from depts", mc);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            Console.WriteLine($"{reader.FieldCount} Columns\n");
+            StringBuilder sb = new StringBuilder();
+            while (reader.Read())
+            {
+                sb.Append($"{reader.GetInt32(0)} " +
+                                  $"{reader.GetString(1)} " +
+                                  $"{reader.GetString(2)} \n");
+            }
+            return sb.ToString();
+        }
+    }
+    
+    public void CreateDept(int deptno, string dname, string loc)
+    {
+        using (MySqlConnection mc = new MySqlConnection(connectionString))
+        {
+            mc.Open();
+            MySqlCommand cmd = new MySqlCommand($"Insert into depts values ({deptno}, '{dname}', '{loc}')", mc);
+            cmd.Parameters.Add("deptno", DbType.Int32).Value = deptno;
+            cmd.Parameters.Add("dbname", DbType.String).Value = dname;
+            cmd.Parameters.Add("loc", DbType.Int32).Value = loc;
+            cmd.ExecuteNonQuery();
+        }
+    }
+    
+    public void DeleteDept(int deptno)
+    {
+        using (MySqlConnection mc = new MySqlConnection(connectionString))
+        {
+            mc.Open();
+            MySqlCommand cmd = new MySqlCommand($"Delete from depts where deptno = {deptno}", mc);
+            cmd.Parameters.Add("deptno", DbType.Int32).Value = deptno;
+            cmd.ExecuteNonQuery();
+        }
+    }
+    
+    public string ReadDept(int deptno)
+    {
+        using (MySqlConnection mc = new MySqlConnection(connectionString))
+        {
+            mc.Open();
+            MySqlCommand cmd = new MySqlCommand($"Select * from depts where deptno = {deptno}", mc);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            StringBuilder sb = new StringBuilder();
+            while (reader.Read())
+            {
+                sb.Append($"{reader.GetInt32(0)} " +
+                                  $"{reader.GetString(1)} " +
+                                  $"{reader.GetString(2)} \n");
+            }
+            return sb.ToString();
+        }
+    }
+    
+    public void UpdateDept(int deptno, string dname, string loc)
+    {
+        using (MySqlConnection mc = new MySqlConnection(connectionString))
+        {
+            mc.Open();
+            MySqlCommand cmd = new MySqlCommand($"Update depts set dname = '{dname}', loc = '{loc}' where deptno = {deptno}", mc);
+            cmd.Parameters.Add("deptno", DbType.Int32).Value = deptno;
+            cmd.Parameters.Add("dbname", DbType.String).Value = dname;
+            cmd.Parameters.Add("loc", DbType.Int32).Value = loc;
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    public void ColorText()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("------------------------------");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("");
+    }
+}
---
+++ ScottConsolenAnwendung2.1/ScottConsolenAnwendung2.1/Program.cs
@@ -0,0 +1,61 @@
+﻿using ScottConsolenAnwendung2._1;
+using System;
+
+
+DBAccess db = new DBAccess();
+
+int count = 0;
+while (true)
+{
+    if (count == 0)
+    {
+        db.ColorText();
+        Console.WriteLine(db.GetAllDepts());
+    }
+    Console.WriteLine("(C)reate (R)ead (U)pdate (D)elete");
+    string menu = Console.ReadLine();
+    if (menu.ToUpper() == "C")
+    {
+        Console.WriteLine("deptno?");
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("dname?");
+        string dn = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("loc?");
+        string l = Convert.ToString(Console.ReadLine());
+        db.CreateDept(d, dn, l);
+        db.ColorText();
+    }
+    else if (menu.ToUpper() == "R")
+    {
+        Console.WriteLine("deptno?");
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(db.ReadDept(d));
+        db.ColorText();
+    }
+    else if (menu.ToUpper() == "U")
+    {
+        Console.WriteLine("deptno?");
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("dname?");
+        string dn = Console.ReadLine();
+        Console.WriteLine("loc?");
+        string l = Console.ReadLine();
+        db.UpdateDept(d, dn, l);
+        db.ColorText();
+    }
+    else if (menu.ToUpper() == "D")
+    {
+        Console.WriteLine("deptno?");
+        int d = Convert.ToInt32(Console.ReadLine());
+        db.DeleteDept(d);
+        db.ColorText();
+    }
+    else
+    {
+        Console.WriteLine(db.GetAllDepts());
+        db.ColorText();
+    }
+    
+
+    count++;
+}
